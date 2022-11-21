0ad8b4dfb958d3751210622baf3475d1139c1adf
JuaniCaste23 <juanicaste23@gmail.com>
2022-11-21 19:16:01 -0300

    subiendo repo a GitHub

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuContentComponent } from './components/menu-content/menu-content.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { ContentAreaComponent } from './components/content-area/content-area.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuContentComponent,
+    ToolbarComponent,
+    ContentAreaComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/menu-content/menu-content.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-content',
+  templateUrl: './menu-content.component.html',
+  styleUrls: ['./menu-content.component.css']
+})
+export class MenuContentComponent {
+
+}
