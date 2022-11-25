c74fa4611b98587f0031e4e81da24277b6997f46
JuaniCaste23 <juanicaste23@gmail.com>
2022-11-24 22:06:38 -0300

    subiendo a GitHub

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { MenuContentComponent } from './components/menu-content/menu-content.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { ContentAreaComponent } from './components/content-area/content-area.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuContentComponent,
     ToolbarComponent,
-    ContentAreaComponent
+    ContentAreaComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
